c4e97026a19604cb90f53b11eca1e006ea91dd87
Daniel Hoberman <daniel.j.hoberman@gmail.com>
2025-09-08 19:33:38 -0400

    fixed small error

--- project/app.py
+++ project/app.py
@@ -18,7 +18,7 @@ hide_function_defs = args.hide_function_defs
 st.set_page_config(page_title="interactive tinytorch")
 st.sidebar.markdown(
     """
-<h1 style="font-size:30pt; float: left; margin-right: 20px; margin-top: 1px;">MiniTorch</h1>{}
+<h1 style="font-size:30pt; float: left; margin-right: 20px; margin-top: 1px;">TinyTorch</h1>{}
 """.format(get_img_tag("https://minitorch.github.io/_images/match.png", width="40")),
     unsafe_allow_html=True,
 )
